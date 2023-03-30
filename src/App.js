f90f52880a299e96e206575e0a536905615b141c
Md Siddiqur Rahman <siddikcoder@gmail.com>
2023-03-30 23:32:19 +0600

    add dnd-kit sortable options

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container } from "react-bootstrap";
+import { DndContext, closestCenter } from "@dnd-kit/core";
+import {
+  arrayMove,
+  SortableContext,
+  verticalListSortingStrategy,
+} from "@dnd-kit/sortable";
+import { useState } from "react";
+import { SortableItem } from "./SortableItem";
 
 function App() {
+  const [languages, setLanguages] = useState([
+    "JavaScript",
+    "PHP",
+    "GO",
+    "Rust",
+    "Java",
+    "Python",
+    "TypeScript",
+  ]);
+
+  const handleDragEnd = (event) => {
+    const { active, over } = event;
+    if (active.id !== over.id) {
+      setLanguages((items) => {
+        const activeIndex = items.indexOf(active.id);
+        const overIndex = items.indexOf(over.id);
+        console.log(arrayMove(items, activeIndex, overIndex));
+        return arrayMove(items, activeIndex, overIndex);
+      });
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <DndContext collisionDetection={closestCenter} onDragEnd={handleDragEnd}>
+      <Container className="p-3" align="center">
+        <h3>The Best Programming Languages</h3>
+        <SortableContext
+          items={languages}
+          strategy={verticalListSortingStrategy}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          {languages.map((language) => (
+            <SortableItem key={language} id={language} />
+          ))}
+        </SortableContext>
+      </Container>
+    </DndContext>
   );
 }
 
